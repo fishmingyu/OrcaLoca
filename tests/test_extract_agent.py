3a5da40834fae5b46dcc73006271953fd5766b9b
Hejia Zhang <hejiazhang2023@outlook.com>
2024-08-30 12:05:43 +0800

    Migrated to pydantic style output in ExtractAgent

--- tests/test_extract_agent.py
+++ tests/test_extract_agent.py
@@ -16,6 +16,7 @@ from Orcar.environment.utils import (
 from Orcar.environment.benchmark import BenchMarkEnv, load_filter_hf_dataset
 
 from Orcar import ExtractAgent
+from Orcar.types import ExtractOutput
 
 logger = get_logger("test_extract_agent")
 
@@ -27,11 +28,11 @@ args_dict = {
     "container_name": "test",
     "split": "test",
     # Short Issue Test
-    #"filter_instance": "^(sympy__sympy-20154)$",
+    "filter_instance": "^(sympy__sympy-20154)$",
     # Long Issue Test
     #"filter_instance": "^(django__django-15814)$",
     # Multi Issue Test
-    "filter_instance": "^(django__django-15814|psf__requests-2317|django__django-13933|sympy__sympy-20154)$",
+    #"filter_instance": "^(django__django-15814|psf__requests-2317|django__django-13933|sympy__sympy-20154)$",
 }
 args = argparse.Namespace(**args_dict)
 cfg = Config("./key.cfg")
@@ -55,10 +56,10 @@ def test_extract_agent():
     agent = ExtractAgent(llm=llm, env=env, verbose=True)
     result_dict = dict()
     for _, inst in env.ds.iterrows():
-        response: AgentChatResponse = agent.chat(json.dumps(dict(inst)))
-        response_json = json.loads(response.response)
-        result_dict[inst['instance_id']] = response_json
-        logger.info(response_json)
+        agent_chat_response: AgentChatResponse = agent.chat(json.dumps(dict(inst)))
+        extract_output = ExtractOutput.parse_raw(agent_chat_response.response)
+        result_dict[inst['instance_id']] = extract_output
+        logger.info(extract_output)
 
     logger.info("Finalizing results:")
     for _, inst in env.ds.iterrows():
