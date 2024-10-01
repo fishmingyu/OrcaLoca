5ae8f677c8ca7b2e22d1ac55f9c61c96ad85a723
Hejia Zhang <hejiazhang2023@outlook.com>
2024-10-01 16:40:50 -0700

    Fixed a bug when not redirecting log

--- tests/test_orcar_agent.py
+++ tests/test_orcar_agent.py
@@ -32,7 +32,7 @@ def test_agent():
     # final_stage = "extract"
     final_stage = "search"
     agent = OrcarAgent(args=args, llm=llm, final_stage=final_stage)
-    agent.set_redirect_log_output(True)
+    # agent.set_redirect_log_output(True)
     for i, inst in enumerate(ds):
         print(f"({i:03d}/{len(ds):03d}) Current inst: {inst['instance_id']}")
         agent.run(dict(inst))
