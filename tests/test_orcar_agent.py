cf1e73adc5d34a6f31e7aa3f94e7bc10564ba5f4
fishmingyu <1661342068@qq.com>
2024-11-22 23:14:35 -0800

    [Test] add editor standalone patch

--- tests/test_orcar_agent.py
+++ tests/test_orcar_agent.py
@@ -16,7 +16,7 @@ args_dict = {
     # Short Issue Test
     # "filter_instance": "^(matplotlib__matplotlib-23314)$",
     # "filter_instance": "^(django__django-15814)$",
-    "filter_instance": "^(mwaskom__seaborn-3010)$",
+    "filter_instance": "^(django__django-11039)$",
     # Long Issue Test
     # "filter_instance": "^(astropy__astropy-6938)$",
     # "filter_instance": "^(astropy__astropy-6938|astropy__astropy-12907)$",
