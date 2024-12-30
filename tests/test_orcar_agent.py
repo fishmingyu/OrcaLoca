52e748a99cc1f23835349f03663954288626e952
fishmingyu <1661342068@qq.com>
2024-12-30 01:25:17 -0800

    test

--- tests/test_orcar_agent.py
+++ tests/test_orcar_agent.py
@@ -31,86 +31,8 @@ args_dict = {
     #     "sympy__sympy-16792|django__django-11999|matplotlib__matplotlib-25332|"
     #     "scikit-learn__scikit-learn-13496)$"
     # ),
-    # whole repo
-    # "filter_instance": ".*",
-    # internal error
-    # "filter_instance": "^(django__django-13315)$",
-    "filter_instance": "^(django__django-17087)$",
-    # Multi Issue Test
-    # "filter_instance": "^(pylint-dev__pylint-7080|matplotlib__matplotlib-26020|pytest-dev__pytest-7490)$",
-    # Wrong action
-    # "filter_instance": (
-    #    "^("
-    #    "django__django-11815|"
-    #    "django__django-13028|"
-    #    "django__django-14999|"
-    #    "django__django-15252|"
-    #    "sympy__sympy-13647|"
-    #    "scikit-learn__scikit-learn-13496"
-    #    ")$"
-    # ),
-    # Test step format
-    # "filter_instance": (
-    #    "^("
-    #    # Wrong step format
-    #    "django__django-11848|"
-    #    "django__django-16139|"
-    #    "django__django-16527|"
-    #    "scikit-learn__scikit-learn-14087|"
-    #    "sympy__sympy-12481|"
-    #    "sympy__sympy-20154|"
-    #    "django__django-15814|"
-    #    # Long issues
-    #    "pylint-dev__pylint-7080|"
-    #    "matplotlib__matplotlib-26020|"
-    #    "pytest-dev__pytest-7490"
-    #    ")$"
-    # ),
-    # Wrong output path format
-    # "filter_instance": (
-    #    "^("
-    #    "astropy__astropy-14995|"
-    #    "django__django-13315|"
-    #    "django__django-13401|"
-    #    "django__django-14855|"
-    #    "sympy__sympy-16792|"
-    #    "sympy__sympy-23262|"
-    #    "sympy__sympy-24066"
-    #    ")$"
-    # ),
-    # Tracer test
-    # "filter_instance": (
-    #    "^("
-    #    "astropy__astropy-12907|"
-    #    "pydata__xarray-4094|"
-    #    "sympy__sympy-18189|"
-    #    "sympy__sympy-18199|"
-    #    "sympy__sympy-21847|"
-    #    "sympy__sympy-24066|"
-    #    "django__django-11815|"
-    #    "scikit-learn__scikit-learn-14983|"
-    #    "sympy__sympy-13647|"
-    #    "sympy__sympy-15345|"
-    #    "sympy__sympy-12419|"
-    #    "sympy__sympy-13031|"
-    #    "sympy__sympy-21379|"
-    #    "sympy__sympy-23262|"
-    #    "astropy__astropy-14995|"
-    #    "django__django-14787|"
-    #    "django__django-16595|"
-    #    "matplotlib__matplotlib-23299|"
-    #    "matplotlib__matplotlib-24149|"
-    #    "sympy__sympy-13480|"
-    #    "sympy__sympy-17139|"
-    #    "sympy__sympy-17655|"
-    #    "astropy__astropy-14182|"
-    #    "astropy__astropy-14365|"
-    #    "scikit-learn__scikit-learn-10297|"
-    #    "sympy__sympy-22714|"
-    #    "matplotlib__matplotlib-25332|"
-    #    "sympy__sympy-17630"
-    #    ")$"
-    # ),
+    # Early Stop Test
+    # "filter_instance": "^(sympy__sympy-17630|django__django-14608|pydata__xarray-4094|sympy__sympy-16792|pytest-dev__pytest-7490|sympy__sympy-18698|django__django-13925|sympy__sympy-21379|django__django-11999|sympy__sympy-17655|django__django-14017|sympy__sympy-15345|sympy__sympy-23262|sympy__sympy-24066|sympy__sympy-17139|astropy__astropy-14182|scikit-learn__scikit-learn-25747|django__django-15814|django__django-15252|sympy__sympy-13031|django__django-13033|sympy__sympy-12419|django__django-13933|django__django-12308|pylint-dev__pylint-7080|sympy__sympy-22714|astropy__astropy-12907|django__django-13028)$",
     "filter_instance": ".*",
 }
 
